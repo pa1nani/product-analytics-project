b7f95767e3c562936ce9301b25ee12f016bd4718
pa1nani <90324239+pa1nani@users.noreply.github.com>
2026-04-10 12:02:33 +0530

    1_funnel_analysis.sql
    -- Funnel Analysis: Tracks user conversion across stages (visit → signup → watch → subscribe)
    -- Calculates conversion percentage at each stage to identify drop-offs in user journey

---
+++ sql/1_funnel_analysis.sql
@@ -0,0 +1,25 @@
+#Funnel Analysis: Tracks user conversion across stages
+        
+WITH funnel AS ( 
+SELECT event_type AS stage,
+	   COUNT(DISTINCT user_id) AS users
+FROM events
+GROUP BY event_type)
+
+SELECT stage,
+	   users,
+       ROUND(users * 100 / FIRST_VALUE(users) OVER ( ORDER BY
+             CASE
+                 WHEN stage = 'visit' THEN 1
+                 WHEN stage = 'signup' THEN 2
+                 WHEN stage = 'watch' THEN 3
+                 WHEN stage = 'subscribe' THEN 4
+			 END),2
+             )AS conversion_percentage
+FROM funnel
+ORDER BY  CASE
+                  WHEN stage = 'visit' THEN 1
+                 WHEN stage = 'signup' THEN 2
+                 WHEN stage = 'watch' THEN 3
+                 WHEN stage = 'subscribe' THEN 4
+			 END;
